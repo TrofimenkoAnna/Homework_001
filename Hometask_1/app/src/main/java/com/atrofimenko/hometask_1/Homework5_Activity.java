929684fef3d7ddbed1088794f74d2f75f75b6b4b
TrofimenkoAnna <trofimenko_anna@ukr.net>
2015-11-07 17:41:05 +0200

    homework5

---
+++ Hometask_1/app/src/main/java/com/atrofimenko/hometask_1/Homework5_Activity.java
@@ -0,0 +1,102 @@
+package com.atrofimenko.hometask_1;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Homework5_Activity extends AppCompatActivity implements Animation.AnimationListener {
+    Animation animFadein;
+    Animation animFadeOut;
+    Animation animZoomIn;
+    Animation animBlink;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework5);
+        Button btn_FadeIn = (Button) findViewById(R.id.btn_FadeIn);
+        Button btn_FadeOut = (Button) findViewById(R.id.btn_FadeOut);
+        Button btn_ZoomIn = (Button) findViewById(R.id.btn_ZoomIn);
+        Button btn_Blink = (Button) findViewById(R.id.btn_Blink);
+        final TextView text_anim = (TextView) findViewById(R.id.text_anim);
+
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+        animFadein.setAnimationListener(this);
+
+        btn_FadeIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                text_anim.setVisibility(View.VISIBLE);
+                text_anim.startAnimation(animFadein);
+            }
+        });
+
+        animFadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+        animFadeOut.setAnimationListener(this);
+
+        btn_FadeOut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                text_anim.setVisibility(View.VISIBLE);
+                text_anim.startAnimation(animFadeOut);
+            }
+        });
+
+        animZoomIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_in);
+        animZoomIn.setAnimationListener(this);
+
+        btn_ZoomIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                text_anim.setVisibility(View.VISIBLE);
+                text_anim.startAnimation(animZoomIn);
+            }
+        });
+
+        animBlink = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        animBlink.setAnimationListener(this);
+
+        btn_Blink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                text_anim.setVisibility(View.VISIBLE);
+                text_anim.startAnimation(animBlink);
+            }
+        });
+    }
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if (animation == animFadein) {
+            Toast.makeText(getApplicationContext(), "Animation Stopped",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+}
--- Hometask_1/app/src/main/java/com/atrofimenko/hometask_1/MainActivity.java
+++ Hometask_1/app/src/main/java/com/atrofimenko/hometask_1/MainActivity.java
@@ -1,52 +1,88 @@
 package com.atrofimenko.hometask_1;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mHomeworkTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button hw1_btn = (Button) findViewById(R.id.hw1_btn);
-        Button hw2_btn = (Button) findViewById(R.id.hw2_btn);
-        Button hw3_btn = (Button) findViewById(R.id.hw3_btn);
-        Button hw4_btn = (Button) findViewById(R.id.hw4_btn);
+  //      setTitle("DrawerLayout");
 
-        hw1_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(MainActivity.this, Homework1_Activity.class);
-                startActivity(intent);
-            }
-        });
+        mHomeworkTitles = getResources().getStringArray(R.array.homework_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        hw2_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(MainActivity.this, Homework2_Activity.class);
-                startActivity(intent);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mHomeworkTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close ) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
             }
-        });
 
-        hw3_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(MainActivity.this, Homework3_Activity.class);
-                startActivity(intent);
+            @Override
+            public void onDrawerOpened(View drawerView) {
             }
-        });
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        hw4_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(MainActivity.this, Homework4_Activity.class);
-                startActivity(intent);
-            }
-        });
     }
+
+    private void selectItem(int position) {
+
+        switch (position) {
+            case 0:
+                startActivity(new Intent(this, Homework1_Activity.class));
+
+                break;
+            case 1:
+                startActivity(new Intent(this, Homework2_Activity.class));
+
+                break;
+            case 2:
+                startActivity(new Intent(this, Homework3_Activity.class));
+                break;
+            case 3:
+                startActivity(new Intent(this, Homework4_Activity.class));
+                break;
+
+            case 4:
+                startActivity(new Intent(this, Homework5_Activity.class));
+                break;
+
+            default:
+                break;
+        }
+    }
+    public class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
 }
